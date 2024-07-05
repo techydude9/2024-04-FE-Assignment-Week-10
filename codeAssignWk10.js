16d8cb2271b1b578fda1bfb57a7956af36a3d05c
techydude9 <bob.ruzga@gmail.com>
2024-07-05 11:17:15 -0400

    Initial base code for Week 10 coding assignment

---
+++ codeAssignWk10.js
@@ -0,0 +1,10 @@
+/*  Week 10 - DOM Coding Assignment
+**  Author: Bob Ruzga
+**  Date: june 29,2024 
+**
+**  Coding Steps:
+Using HTML, Bootstrap, and JavaScript create a single page website that contains the following:
+    A Bootstrap styled table representing your choice of data.
+    A Bootstrap styled form that allows a user to add a new row to the table when clicking on submit.
+*/
+
