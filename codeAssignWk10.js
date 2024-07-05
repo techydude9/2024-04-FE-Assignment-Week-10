e28ce542ef8fb2e366e7bc6f7c178d6dc724404c
techydude9 <bob.ruzga@gmail.com>
2024-07-05 17:02:08 -0400

    Nearly complete - cant remove an entry

--- codeAssignWk10.js
+++ codeAssignWk10.js
@@ -8,3 +8,34 @@ Using HTML, Bootstrap, and JavaScript create a single page website that contains
     A Bootstrap styled form that allows a user to add a new row to the table when clicking on submit.
 */
 
+let id = 0;
+
+document.getElementById('addMusic').addEventListener('click', () => {
+    let table = document.getElementById('mTable1');
+    let row = table.insertRow(1);
+    row.setAttribute('rowId', `item-${id}`);
+    row.insertCell(0).innerHTML = id + 1;
+    row.insertCell(1).innerHTML = document.getElementById('genreId').value;
+    row.insertCell(2).innerHTML = document.getElementById('bandId').value;
+    row.insertCell(3).innerHTML = document.getElementById('albumId').value;
+    let rowAction = row.insertCell(4);
+    rowAction.appendChild(createRemoveBtn(id++));
+    document.getElementById('genreId').value = ' ';
+    document.getElementById('bandId').value = ' ';
+    document.getElementById('albumId').value = ' ';
+    })
+
+function createRemoveBtn(id) {
+    let rmBtn = document.createElement('button');
+    rmBtn.className = 'btn btn-primary';
+    rmBtn.id = id;
+    rmBtn.innerHTML = 'Remove';
+    rmBtn.onclick = () => {
+        console.log(`Removing row with id: item-${id}`);
+        let elementToRemove = document.getElementById(`item-${id}`);
+        console.log(elementToRemove);
+        elementToRemove.parentNode.removeChild(elementToRemove);
+    };
+    return rmBtn;
+
+}
